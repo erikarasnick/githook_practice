38538fe29eb0025ea3d9f8544b8a4ae5508b65ab
Erika <erika.rasnick@gmail.com>
2021-06-29 22:07:24 -0400

    1st commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# githook_practice
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of githook_practice is to ...
+
+```
+docker run --rm -v $PWD:/tmp degauss/pm:0.1.1 my_address_file_geocoded.csv
+```
---
+++ pre-push.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+# make sure to change version number in readme
+
+ans1 <- readline("Did you update the version number in the readme example Docker call (y/n)? ")
+if (!ans1 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+
+ans2 <- readline("Did you update the version number in the DeGAUSS greeting message (y/n)? ")
+if (!ans2 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+
+ans3 <- readline("Did you update the version number for the saved file name (y/n)? ")
+if (!ans3 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
