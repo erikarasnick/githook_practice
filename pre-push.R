2e52df3ab83432b3212974ac83eaeb6f708b5276
Erika <erika.rasnick@gmail.com>
2021-06-29 22:17:19 -0400

    use file.mtime

--- pre-push.R
+++ pre-push.R
@@ -1,12 +1,17 @@
 #!/usr/bin/env Rscript
 
-# make sure to change version number in readme
+# make sure to update version number
 
-ans1 <- readline("Did you update the version number in the readme example Docker call (y/n)? ")
-if (!ans1 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+if ( file.mtime("version.R") < file.mtime("README.md") ) {
+  stop("Did you update the version number?")
+}
 
-ans2 <- readline("Did you update the version number in the DeGAUSS greeting message (y/n)? ")
-if (!ans2 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+# ans1 <- readline("Did you update the version number in the readme example Docker call (y/n)? ")
+# if (!ans1 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
 
-ans3 <- readline("Did you update the version number for the saved file name (y/n)? ")
-if (!ans3 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+# ans2 <- readline("Did you update the version number in the DeGAUSS greeting message (y/n)? ")
+# if (!ans2 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+#
+# ans3 <- readline("Did you update the version number for the saved file name (y/n)? ")
+# if (!ans3 %in% c("", "y", "Y")) stop("aborted", call. = FALSE)
+
